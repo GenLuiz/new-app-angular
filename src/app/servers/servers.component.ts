ccf61a928ee5b7fc30b50784778520e068e1582a
Barbosa, Luiz <luiz.barbosa@wipro.com>
2017-09-19 18:23:33 -0300

    section 2 lect 21

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: 'app-servers', element html
+  // selector: '[app-servers]' atributo de uma tag e.g <div app-servers></div>
+  // selector: '.app-servers' classe html e.g <div class="app-servers"></div>
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
